61dcdb270a87121168f8d9c14575f867f96019c7
sraju728@gmail.com <chaitu2108@gmail.com>
2018-09-27 13:01:44 +0530

    creating add contacts function

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from add import AddContact
+
 class Main:
 	ContactNames = ['Keanu Reeves', 'Brad Pitt', 'Trisha', 'Vijay Sethupathy']
     ContactNumbers = [9685768789, 9687783421, 9743274880, 9949083597]
@@ -7,3 +9,12 @@ class Main:
     Data = dict(zip(ContactNames, ContactNumbers))
 
     AlphabetDict = {0: [' '], 1: [''], 2: ['a', 'b', 'c'], 3: ['d', 'e', 'f'], 4:['g', 'h', 'i'], 5:['j','k','l'], 6:['m','n','o'], 7:['p','q','r','s'], 8:['t','u','v'], 9:['w','x','y','z']}
+	
+	print('1.Adding contact, 2. Search contact')
+
+    preference = input('Enter your preference : ')
+
+    if(preference == 1):
+        name = raw_input('Enter name of the new contact : ')
+        number = input('Enter number of the new Number : ')
+        AddContact(name, number, Data)
