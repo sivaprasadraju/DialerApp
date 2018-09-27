f7e14bedc1730025b4e31a7c26131925856984c9
sraju728@gmail.com <chaitu2108@gmail.com>
2018-09-27 12:55:27 +0530

    Adding testcase for Data dictionary

--- test_main.py
+++ test_main.py
@@ -4,3 +4,6 @@ from main import Main
 class TestMain(TestCase):
     def TestCaseForAlphabetDictDictionary(self):
         self.assertEqual(Main().AlphabetDict[2], ['a', 'b', 'c'])
+		
+	def TestCaseForDataDictionary(self):
+        self.assertEqual(Main().Data['Trisha'], 9743274880)
